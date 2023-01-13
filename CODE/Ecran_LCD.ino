a49633b64972d55c8147b8a66fdc82ac6e868a27
Patiejunas Edvinas <119620232+pe106026@users.noreply.github.com>
2023-01-13 15:45:56 +0100

    Update Ecran_LCD.ino

--- CODE/Ecran_LCD.ino
+++ CODE/Ecran_LCD.ino
@@ -1,14 +1,10 @@
-//YWROBOT
-//Compatible with the Arduino IDE 1.0
-//Library version:1.1
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27,20,4);
 
 void setup()
 {
   lcd.init();
-  // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(3,0);
   lcd.print("Salut Zizou!");
